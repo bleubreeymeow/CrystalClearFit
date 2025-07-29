a302a1b6e7f36ac52c1c115545e53777165a85a1
bleubreeymeow <alris.dai2003@gmail.com>
2025-07-29 16:40:19 +0200

    commit

--- alrisDistortionFit/PBCO/alris_functions2.py
+++ alrisDistortionFit/PBCO/alris_functions2.py
@@ -14,9 +14,9 @@ def transform_list_hkl_p63_p65(hkl_list):
     hkl_list = tf.convert_to_tensor(hkl_list, dtype=tf.float32)
 
     # Apply the transformation using TensorFlow operations
-    h_new = - hkl_list[:, 0] /9
+    h_new = -9 * hkl_list[:, 0]
     k_new = hkl_list[:, 2]
-    l_new = hkl_list[:, 1] / 9
+    l_new = 9 *hkl_list[:, 1] 
 
     # Stack the new h, k, l components into a single tensor
     result = tf.stack([h_new, k_new, l_new], axis=1)
@@ -99,7 +99,7 @@ def get_atomic_form_factor(qnorm, atom):
     return fq
 
 
-def get_structure_factors(hkl_batch, structure):
+def get_structure_factors(hkl_batch, structure , hkl_parent):
     """
     Vectorized structure factor calculation.
 
